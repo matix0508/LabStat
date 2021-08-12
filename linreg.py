a71ead5b56c35695a140bfb66f3c4e3dc12f98da
Mateusz Wlekliński <matix0508@gmail.com>
2021-08-12 12:04:18 +0200

    init linreg

---
+++ linreg.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+
+import statsmodels.api as sm
+from uncertainties import ufloat
+import numpy as np
+
+class LinReg:
+    def __init__(self, x: np.ndarray, y: np.ndarray) -> None:
+        self.x = sm.add_constant(x)
+        self.y = y
+        self.model = sm.OLS(self.y, self.x)
+        self.results = self.model.fit()
+        self.a, self.b = self.get_a_b()
+
+
+    def get_a_b(self) -> Tuple[ufloat]:
+        a = self.results.params[1]
+        b = self.results.params[0]
+        ua = self.results.bse[1]
+        ub = self.results.bse[0]
+        a = ufloat(a, ua)
+        b = ufloat(b, ub)
+        return (a, b)
+
+    def __repr__(self) -> str:
+        return f"y = ({self.a:.2e})x + ({self.b:.2e})"
+    
+
+
+    
