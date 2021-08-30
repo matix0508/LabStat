f08ab66a9144d48124c49cc868b71340394c748c
Mateusz Wlekliński <matix0508@gmail.com>
2021-08-30 12:35:15 +0200

    curve_fitting

---
+++ curve_fit.py
@@ -0,0 +1,40 @@
+from operator import inv
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import numpy as np
+from typing import List
+from uncertainties import ufloat
+
+def inverse_square(x, a):
+    return a/x**2
+
+
+class CurveFit:
+    def __init__(self, x: np.ndarray, y: np.ndarray, func: 'function', parameters: List[str]) -> None:
+        self.x = x
+        self.y = y
+        self.func = func
+        self.parameters = parameters
+        self.output = {}
+        self.popt = None
+        self.pcov = None
+
+    def find_curve(self):
+        self.popt, self.pcov = curve_fit(self.func, self.x, self.y)
+        for i, parameter in enumerate(self.parameters):
+            self.output[parameter] = ufloat(self.popt[i], self.pcov[i])
+        
+    def plot(self):
+        xdata = np.linspace(self.x.min(), self.x.max(), 100)
+        plt.scatter(self.x, self.y)
+        plt.plot(xdata, self.func(xdata, self.output['a'].nominal_value))
+        plt.show()
+
+xdata = np.array([1, 2, 3, 4, 5, 6])
+ydata = inverse_square(xdata, 3)
+
+cd = CurveFit(xdata, ydata, inverse_square, ['a'])
+cd.find_curve()
+cd.plot()
+
+# popt, pcov = curve_fit(inverse_square, xdata, ydata)
